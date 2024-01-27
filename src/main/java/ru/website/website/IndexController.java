ddba93663a5114960f8de59785f96565f447d9ea
Sn0wled <misha.safin.sn0wled@gmail.com>
2024-01-28 00:52:24 +0500

    Сделал через AJAX

--- src/main/java/ru/website/website/IndexController.java
+++ src/main/java/ru/website/website/IndexController.java
@@ -1,10 +1,8 @@
 package ru.website.website;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 
@@ -19,46 +17,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class IndexController {
-    List<Disc> discs = new ArrayList<>();
-    public  IndexController(){
-        discs.addAll( Arrays.asList(
-            new Disc("Bat Out of Hell", "img/bat_out_of_hell.jpg", "123", Genre.POPULAR),
-            new Disc("Greatest Hits", "img/greatest_hits.jpg", "123", Genre.POPULAR),
-            new Disc( "The Woman in Me", "img/the-woman-in-me.png", "123", Genre.POPULAR),
-            new Disc( "Born in the U.S.A.", "img/born-in-the-usa.jpg", "123", Genre.POPULAR),
-            new Disc( "Чайковский. На все времена", "img/chaykovsky.jpeg", "123", Genre.CLASSIC),
-            new Disc( "Моцарт. 46 симфоний", "img/mozart.jpeg", "123", Genre.CLASSIC),
-            new Disc( "Vivaldi. Concerti per violino Violoncello e Archi", "img/vivaldi.jpg", "123", Genre.CLASSIC),
-            new Disc( "The Best of Chopin", "img/chopin.jpg", "123", Genre.CLASSIC),
-            new Disc( "Mojo Helden", "img/mojo_helden.jpg", "123", Genre.BLUS),
-            new Disc( "Bottleneck Sessions", "img/bottleneck_sessions.jpg", "123", Genre.BLUS),
-            new Disc( "Talkin' That Talk", "img/talkin-that-talk.jpg", "123", Genre.BLUS),
-            new Disc( "Two Pines", "img/two-pines.jpg", "123", Genre.BLUS),
-            new Disc( "Fly or Die Fly or Die Fly or Die", "img/fly-or-die.png", "123", Genre.JASS),
-            new Disc( "Urban Jazzscape", "img/urban-jazzscape.jpg", "123", Genre.JASS),
-            new Disc( "My Sixties in Jazz", "img/my-sixties-in-jazz.jpg", "123", Genre.JASS),
-            new Disc( "Late Night Improvs", "img/late-night-improvs.jpg", "123", Genre.JASS),
-            new Disc( "Burning Castles", "img/burning-castles.jpg", "123", Genre.ROCK),
-            new Disc( "Recurring", "img/recurring.jpg", "123", Genre.ROCK),
-            new Disc( "Severance", "img/severance.jpg", "123", Genre.ROCK),
-            new Disc( "Electric Sounds", "img/electric-sounds.jpg", "123", Genre.ROCK),
-            new Disc( "IDENTITY", "img/identity.jpg", "123", Genre.METALL),
-            new Disc( "A Dark Euphony", "img/a-dark-euphony.jpg", "123", Genre.METALL),
-            new Disc( "Everfrost", "img/everfrost.jpg", "123", Genre.METALL),
-            new Disc( "Imperium", "img/imperium.jpg", "123", Genre.METALL),
-            new Disc( "Midlife Crisis", "img/midlife-crisis.jpg", "123", Genre.HIPHOP),
-            new Disc( "BasketCase", "img/basket-case.jpg", "123", Genre.HIPHOP),
-            new Disc( "Pulingo", "img/pulingo.jpg", "123", Genre.HIPHOP),
-            new Disc( "Ringue da Vida", "img/ringua.jpg", "123", Genre.HIPHOP),
-            new Disc( "Step by Step", "img/step-by-step.jpg", "123", Genre.COUNTY),
-            new Disc( "The Moment", "img/the-moment.jpg", "123", Genre.COUNTY),
-            new Disc( "Melbourne Sentimental", "img/melbourne-sentimental.jpg", "123", Genre.COUNTY),
-            new Disc( "Jeans On", "img/jeans-on.jpg", "123", Genre.COUNTY)
-        ));
-        for (int i = 0; i < discs.size(); i++){
-            discs.get(i).setId(i);
-        }
-    }
+    @Autowired
+    private DiscContainer discContainer;
 
     @GetMapping("/")
     public String getMethodName() {
@@ -69,23 +29,14 @@ public class IndexController {
     public void AddDiscs(Model model){
         Genre[] genres = Genre.values();
         for (Genre genre : genres){
-            model.addAttribute(genre.toString().toLowerCase(), filterByGenres(discs, genre));
+            model.addAttribute(genre.toString().toLowerCase(), discContainer.filterByGenres(genre));
         }
-    }
-
-    private Iterable<Disc> filterByGenres(
-        List<Disc> discs, Genre genre
-    ){
-        return discs.stream()
-        .filter(x -> x.getGenre().equals(genre))
-        .collect(Collectors.toList());
     }
 
     @PostMapping
     public String postMethodName(int id, Model model) {
-        discs.removeIf(x -> x.getId()==id);
+        discContainer.removeDisc(id);
         AddDiscs(model);
         return "index1";
     }
-    
 }
--- src/main/resources/static/js/hide.js
+++ src/main/resources/static/js/hide.js
@@ -5,3 +5,23 @@ for (let i = 0; i < asd.length; i++) {
         asd[i].parentNode.classList.toggle('hidden');
     }
 }
+
+let buttons = document.querySelectorAll('.to-cart');
+for (let i = 0; i < buttons.length; i++){
+    buttons[i].onclick = async function(){
+        let response = await fetch("/to_cart?id="+this.value, {method: 'POST'});
+
+        if (response.ok) { // если HTTP-статус в диапазоне 200-299
+            // получаем тело ответа (см. про этот метод ниже)
+            let json = await response.json();
+            console.log(json);
+            if (json.result){
+                let child = this.parentElement;
+                let parent = child.parentElement;
+                parent.removeChild(child);
+            }
+          } else {
+            alert("Ошибка HTTP: " + response.status);
+          }
+    }
+}
